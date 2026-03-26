3677203b564df61e80d38e1bc040a90116983804
vx59 <derosajb19@gmail.com>
2026-03-25 22:50:43 -0400

    fixing spelling mistake loll

---
+++ maintenance/canonicalize-names.py
@@ -0,0 +1,49 @@
+from sqlalchemy import update, func
+from musiql_api.models import MusiqlRepository
+from musiql_api.db import async_session
+import asyncio
+
+async def canonicalize_names():
+
+    clean_stmt = (
+        update(MusiqlRepository)
+        .values(
+            title=func.regexp_replace(
+                func.regexp_replace(
+                    func.regexp_replace(
+                        MusiqlRepository.title,
+                        r'^.*[-–]\s*',
+                        ''
+                    ),
+                    r'[\(\[]\s*.*?(official|visualizer|video|audio|hd).*?[\)\]]',
+                    '',
+                    'i'
+                ),
+                r'^.*-\s*',
+                ''
+            )
+        )
+    )
+
+    split_stmt = (
+        update(MusiqlRepository)
+        .values(
+            title=func.regexp_replace(
+                MusiqlRepository.title,
+                r'^.*-\s*',
+                ''
+            )
+        )
+    )
+
+    async with async_session() as session:
+        await session.execute(clean_stmt)
+        await session.execute(split_stmt)
+
+        await session.commit()
+
+async def main():
+    await canonicalize_names()
+
+if __name__ == "__main__":
+    asyncio.run(main())
