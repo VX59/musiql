51dd32f0b1cf5c61d02331bb0fe25f3a5f9ea274
vx59 <derosajb19@gmail.com>
2026-03-30 19:50:55 -0400

    pydantic settings, dockerfile

--- musiql_api/db.py
+++ musiql_api/db.py
@@ -1,16 +1,32 @@
 from sqlalchemy.orm import sessionmaker
 from configparser import ConfigParser
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
-import os
-config = ConfigParser()
-config.read("alembic.ini")
-password = os.getenv("DB_PASSWORD")
-DATABASE_URL = f"postgresql+asyncpg://jacob:{password}@jacob-server:5432/musiql"
+from functools import lru_cache
 
-engine = create_async_engine(DATABASE_URL, echo=True)
+from musiql_api.settings import get_settings, Settings
 
-async_session = sessionmaker(
-    bind=engine,
-    class_=AsyncSession,
-    expire_on_commit=False,
-)
+@lru_cache
+def get_engine():
+
+    settings:Settings = get_settings()
+
+    config = ConfigParser()
+    config.read("alembic.ini")
+    password = settings.db_password
+    DATABASE_URL = f"postgresql+asyncpg://jacob:{password}@jacob-server:5432/musiql"
+
+    engine = create_async_engine(DATABASE_URL, echo=True)
+
+    return engine
+
+@lru_cache
+def get_session():
+    engine = get_engine()
+
+    async_session = sessionmaker(
+        bind=engine,
+        class_=AsyncSession,
+        expire_on_commit=False,
+    )
+
+    return async_session
