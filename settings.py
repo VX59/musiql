2775fcb6a13c037a2a43cda56d1dbb6a47d8725a
vx59 <derosajb19@gmail.com>
2026-05-06 22:12:20 -0400

    adding aws resources for production app

--- settings.py
+++ settings.py
@@ -13,7 +13,6 @@ class Settings(BaseSettings):
     db_password: str
     db_domain: str
     musiql_api_url: str
-    media_ingestion_api_url: str
     jwt_secret_key: str
 
     aws_region: str = "us-east-2"
