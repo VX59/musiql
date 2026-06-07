79a27eafc402583de926b76e9715b987bee288c9
vx59 <derosajb19@gmail.com>
2026-06-06 22:40:49 -0400

    timer_log prefix

--- utility.py
+++ utility.py
@@ -105,7 +105,7 @@ def retry(code_holder, label: str = ""):
 
 class timer_log:
     def __init__(self, label: str = "", extra: dict = {}):
-        self.label = label
+        self.label = "timer_log -" + label
         self.logger = logger
         self.extra = extra
 
